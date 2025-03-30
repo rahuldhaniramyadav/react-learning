7eed3f90fe6682e4fd61cdb2dbe86c948201680e
Rahul <rahul.d.yadav@globallogic.com>
2025-03-30 15:38:09 +0530

    react-learning: update the data based on that state

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,37 @@
+import { useState } from "react";
 import restrautArr from "../utilis/restaurantdata";
 import RestaurantCard from "./RestaurantCard";
 
 const Main = () => {
+
+  // local state variable - super powerful variable
+  const [restList, setRestList] = useState(restrautArr);
+
+  // const restArray = useState(restrautArr);
+  // const [restList, setRestList] = restArray;
+  // const restList  =  restArray[0];
+  // const setRestList  =  restArray[1];
+
+  const filterFun = () => {
+    const result  = restList.filter((rest) => {
+      return rest.data.avgRating > 3;
+    })
+    setRestList(result);
+  }
+
   return (
     <main>
+      <button  onClick={() => {
+        const result  = restList.filter((rest) => {
+        return rest.data.avgRating > 4;
+      })
+      setRestList(result);
+      }}>Filter </button>
+      <button onClick={filterFun}>Filter with create function name</button>
       <div className="res-container">
         {
-          restrautArr.map((restrautItem) => <RestaurantCard key={restrautItem.data.id} resData={restrautItem} />)
+          // restrautArr.map((restrautItem) => <RestaurantCard key={restrautItem.data.id} resData={restrautItem} />)
+          restList.map((restrautItem) => <RestaurantCard key={restrautItem.data.id} resData={restrautItem} />)
         }
         {/* <RestaurantCard resData = {restrautArr[0]} /> */}
       </div>
