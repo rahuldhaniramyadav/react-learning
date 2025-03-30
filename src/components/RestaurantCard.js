80a42922cde16a2f1168b8e414704b35e332f3ae
Rahul <rahul.d.yadav@globallogic.com>
2025-03-30 15:35:27 +0530

    react-learning: updated the constant name and fetching value

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,5 +1,5 @@
-// import Rahu, {REST_IMAGE, LOGO_URL} from "../utilis/constant";
-import {REST_IMAGE} from "../utilis/constant";
+// import Rahu, {CDN_IMAGE, LOGO_URL} from "../utilis/constant";
+import {CDN_IMAGE} from "../utilis/constant";
 
 const RestaurantCard = (pro) => {
 
@@ -7,18 +7,18 @@ const RestaurantCard = (pro) => {
   // console.log(pro.resData);
   // console.log(pro.resData.data);
 
-  const { name, cuisines, costForTwo,  totalRatings, cloudinaryImageId} = pro.resData.data;
+  const { name, cuisines, costForTwo,  avgRating, cloudinaryImageId} = pro.resData.data;
 
   // const { resData } = ;
   // console.log(resData);
   // console.log(resData.data);
   return (
     <div className="res-card">
-      <img className="res-image" src={REST_IMAGE+cloudinaryImageId}  alt="resaurant" />
+      <img className="res-image" src={CDN_IMAGE+cloudinaryImageId}  alt="resaurant" />
       <h3>{name}</h3>
       <h4>{cuisines.join(', ')}</h4>
       <span>₹{costForTwo / 100} For TWO</span>
-      <p>{totalRatings / 1000}</p>
+      <p>{avgRating}</p>
       {/* {LOGO_URL}
       <Rahu /> */}
     </div>
--- src/utilis/constant.js
+++ src/utilis/constant.js
@@ -1,6 +1,6 @@
 export const LOGO_URL  = "https://st2.depositphotos.com/40002406/42721/v/450/depositphotos_427216920-stock-illustration-food-delivery-logo-template-design.jpg"
 
-export const REST_IMAGE = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_292,h_300/";
+export const CDN_IMAGE = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_292,h_300/";
 
 const Rahu = () => {
   return(
