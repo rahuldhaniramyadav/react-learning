e079e4003bd334c4d6e01e56115831e1a2e7d12c
Rahul <rahul.d.yadav@globallogic.com>
2025-03-31 21:06:39 +0530

    react-learning: class based component didmount

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,32 @@
 import UserFun from "./UserFun";
 import UserClass from "./UserClass";
 import "../utilis/about.module.css"; // need to check how we can import css file for the single component
+// import { Component } from "react"; // it shoudl work but it is not working
+import React from "react";
 
-const About = () => {
-  return(
-    <div>
-      <h1>About us </h1>
-      <UserFun name={" Function "} location={" Fucntion INDIA  "} />
-      <UserClass name={" Class "} location={" Class INDIA "} />
-    </div>
-  )
+//change the About to class based component from function based
+class About extends React.Component {
+  // whever class instance is created it will trigger 
+  constructor(prop) {
+    super(prop);
+    console.log("Parent Constructor");
+  }
+
+  // At the end componentDidMount but here we have child component to load the child didmount will be called first and than parent
+  componentDidMount() {
+    console.log("Parent componentDidMount");
+  }
+
+  render() {
+    console.log("Parent Render");
+    return(
+      <div>
+        <h1>About us </h1>
+        <UserFun name={" Function "} location={" Fucntion INDIA  "} />
+        <UserClass name={" Class "} location={" Class INDIA "} />
+      </div>
+    )
+  }
 }
 
 export default About;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -9,8 +9,16 @@ class UserClass extends React.Component {
       count: 0,
       count2: 1,
     }
+    console.log("Child Constructor");
   }
+
+  // At the end componentDidMount in the class so basically it is used for the api call 
+  componentDidMount() {
+    console.log("Child componentDidMount");
+  }
+
   render() {
+    console.log("Child Render");
     const {name, location} = this.props;
     const {count, count2} = this.state;
     return(
@@ -18,6 +26,11 @@ class UserClass extends React.Component {
         <div>
           <div>Count : {count}</div>
           <div>Count2 : {count2}</div>
+          <button onClick={() => {
+            this.setState({
+              count: this.state.count+1
+            })
+          }}>Increment Count</button>
         </div>
         <h1>{name}</h1>
         <h2>{location}</h2>
