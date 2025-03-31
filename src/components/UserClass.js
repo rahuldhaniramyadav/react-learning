793b923eb357629777d93865c8dc48a5a7aaae59
Rahul <rahul.d.yadav@globallogic.com>
2025-03-31 19:17:55 +0530

    react-learning: add and access state in class based component

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -3,11 +3,22 @@ import React from "react"
 class UserClass extends React.Component {
   constructor(props) {
     super(props); //super(props) is used in the constructor method of a class to call the constructor of the parent class (React.Component)
+
+    // how we can create state variable
+    this.state = {
+      count: 0,
+      count2: 1,
+    }
   }
   render() {
     const {name, location} = this.props;
+    const {count, count2} = this.state;
     return(
-      <div>
+      <div className="class-based">
+        <div>
+          <div>Count : {count}</div>
+          <div>Count2 : {count2}</div>
+        </div>
         <h1>{name}</h1>
         <h2>{location}</h2>
       </div>
--- src/components/UserFun.js
+++ src/components/UserFun.js
@@ -1,7 +1,17 @@
+import { useState } from "react";
+
 const UserFun = ({name, location}) => {
 
+  const [count] = useState(0);
+  const [count2] = useState(2);
+
+
   return(
-    <div>
+    <div className="function-based">
+      <div>
+        <div>Count : {count}</div>
+        <div>Count2 : {count2}</div>
+      </div>
       <h1>{name}</h1>
       <h2>{location}</h2>
     </div>
