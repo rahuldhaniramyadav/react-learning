a00a42996ecd48e2905c63678ab8546cc9321fe3
Rahul <rahul.d.yadav@globallogic.com>
2025-04-02 11:54:23 +0530

    react-learning: added react helment to check whether we can change the head element of dom

--- src/components/About.js
+++ src/components/About.js
@@ -4,6 +4,7 @@ import "../utilis/about.module.css"; // need to check how we can import css file
 // import { Component } from "react"; // it shoudl work but it is not working
 import React from "react";
 import NewUserClass from "./NewUserClass";
+import { Helmet } from 'react-helmet'; // using to check can we modify the head element
 
 //change the About to class based component from function based
 class About extends React.Component {
@@ -22,6 +23,10 @@ class About extends React.Component {
     console.log("Parent Render");
     return(
       <div>
+        <Helmet>
+          <title>My Dynamic Page Title</title>
+          <meta name="description" content="This is a about us of my page added by Helmet component" />
+        </Helmet>
         <h1>About us </h1>
         <UserFun name={" Function "} location={" Fucntion INDIA  "} />
         {/* The dom manipulation will be happend on batches once the contructor, render method of class component is called */}
