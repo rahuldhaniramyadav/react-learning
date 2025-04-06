d8c3d953b698f44faa2a8c1f5db1116f85ee9ec2
Rahul <rahul.d.yadav@globallogic.com>
2025-04-06 15:30:23 +0530

    react-learning: added describe which consider as section and using it replacing test

--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -2,42 +2,46 @@ import { render, screen } from "@testing-library/react";
 import Contact from "../Contact";
 import "@testing-library/jest-dom";
 
-test("should load the contact compoennt", () => {
-  // render(<Contact />);
 
-  // //querying
-  // const heading = screen.findByRole("heading");
-  // console.log(heading);
+describe("contact us page ", () => {
 
-  // //assertion
-  // expect(heading).toBeInTheDocument();
-
-  const { getByText } = render(<Contact />);
-  const element = getByText('Contact US'); // This will return an HTMLElement if it exists.
-  expect(element).toBeInTheDocument(); 
-
-})
-
-test("should load the all input", () => {
-  const { getAllByRole } = render(<Contact />);
-
-  //querying
-  const inpElement = getAllByRole("textbox");
-  // console.log(inpElement);
-
-  //assertion
-  expect(inpElement.length).toBe(3);
-
-})
-
-test("should submit button", () => {
-  const { getByText } = render(<Contact />);
-
-  //querying
-  const submit = getByText("Submit");
-  // console.log(inpElement);
-
-  //assertion
-  expect(submit).toBeInTheDocument();
-
+    it("should load the contact compoennt", () => {
+      // render(<Contact />);
+    
+      // //querying
+      // const heading = screen.findByRole("heading");
+      // console.log(heading);
+    
+      // //assertion
+      // expect(heading).toBeInTheDocument();
+    
+      const { getByText } = render(<Contact />);
+      const element = getByText('Contact US'); // This will return an HTMLElement if it exists.
+      expect(element).toBeInTheDocument(); 
+    
+    })
+    
+    it("should load the all input", () => {
+      const { getAllByRole } = render(<Contact />);
+    
+      //querying
+      const inpElement = getAllByRole("textbox");
+      // console.log(inpElement);
+    
+      //assertion
+      expect(inpElement.length).toBe(3);
+    
+    })
+    
+    it("should submit button", () => {
+      const { getByText } = render(<Contact />);
+    
+      //querying
+      const submit = getByText("Submit");
+      // console.log(inpElement);
+    
+      //assertion
+      expect(submit).toBeInTheDocument();
+    
+    })
 })
